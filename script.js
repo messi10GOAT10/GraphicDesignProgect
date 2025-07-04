1e28298dbf670789dcfd322b0530e57c2894c9eb
sergey123423213 <grinya.0895@gmail.com>
2025-07-04 22:57:50 +0300

    edit burger js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const openModal = document.querySelector('.reviews-cooperation-container-contact')
 const overlay = document.querySelector('.overlay')
+const navBurgermenuForm = document.querySelector('.nav-burgermenu-form')
+const navBurgermenu = document.querySelector('.nav-burgermenu')
+
+navBurgermenuForm.addEventListener('click',function(){
+    navBurgermenu.style.display = 'flex'
+})
+document.addEventListener('keydown',function(e){
+    if(e.key == 'Escape'){
+        navBurgermenu.style.display = 'none'
+    }
+})
+
 openModal.addEventListener('click', function(){
     overlay.style.display = 'flex';
 })
-overlay.addEventListener('click',function(){
+overlay.addEventListener('click',function(e){
+    alert('Closed')
     overlay.style.display = 'none';
 })
